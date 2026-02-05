11255547e0cee2cb4f51d19c7afb9c0e26e5cb4e
Antoniosvj <antoniosvj@hotmail.com>
2026-02-04 21:49:57 -0300

    Inicializei o repositorio

---
+++ src/main/java/org/Antoniosvj/ItemCardapio.java
@@ -0,0 +1,13 @@
+package org.Antoniosvj;
+
+// classe funciona como molde do objeto
+public class ItemCardapio {
+    //atributos são as características
+    long id;
+    String nome;
+    boolean emPromocao;
+    double preco;
+    double precoComDesconto;
+    int categoria;
+
+}
