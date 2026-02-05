c5efa048c6249f34392614f58e4775c96480d4ca
Antoniosvj <antoniosvj@hotmail.com>
2026-02-04 23:11:57 -0300

    aula sobre constructores e metodos

--- src/main/java/org/Antoniosvj/ItemCardapio.java
+++ src/main/java/org/Antoniosvj/ItemCardapio.java
@@ -11,4 +11,41 @@ public class ItemCardapio {
     double precoComDesconto;
     int categoria;
 
+    //metodos
+    double calculaPorcentagemDesconto(){
+        return (preco - precoComDesconto) / preco;
+    }
+
+    String obtemCategoria(){
+        String nomeCategoria;
+        switch (categoria){
+            case 1:
+                nomeCategoria = "Bebidas";
+                break;
+            case 2:
+                nomeCategoria = "Pratos Principais";
+                break;
+            case 3:
+                nomeCategoria = "Sobremesa";
+                break;
+            default:
+                nomeCategoria = "Não existe...";
+                break;
+        }
+        return nomeCategoria;
+    }
+
+    void definePromocao(double precoComDesconto){
+        this.emPromocao = true;
+        this.precoComDesconto = precoComDesconto;
+    }
+
+    //construtor
+    ItemCardapio(long id, String nome, String descricao, double preco, int categoria){
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
 }
--- src/main/java/org/Antoniosvj/Main.java
+++ src/main/java/org/Antoniosvj/Main.java
@@ -4,6 +4,7 @@ package org.Antoniosvj;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        /*
         ItemCardapio item1 = new ItemCardapio();
         item1.id = 1;
         item1.nome = "Refresco do Chaves";
@@ -11,7 +12,9 @@ public class Main {
         item1.emPromocao = false;
         item1.preco = 1.99;
         item1.categoria = 1;
-
+        */
+        ItemCardapio item1 = new ItemCardapio(1, "Refresco do Chaves", "Suco de limão que parece tamarindo e tem gosto de groselia", 1.99, 1);
+        /*
         ItemCardapio item2 = new ItemCardapio();
         item2.id = 2;
         item2.nome = "Sanduiche de presunto do Chaves";
@@ -22,6 +25,20 @@ public class Main {
         item2.categoria = 2;
 
 
-        IO.println("Nome: " + item1.nome);
+        *
+        * IO.println("Nome: " + item2.nome);
+        double porcentagemDesconto = item2.calculaPorcentagemDesconto();
+        IO.println("Porcentagem de desconto? " + porcentagemDesconto);
+
+        IO.println("Categoria: " + item2.obtemCategoria());
+        * */
+
+        ItemCardapio item2 = new ItemCardapio(2, "Sanduiche de presunto do Chaves","Sanduiche de presunto simples", 3.50, 2 );
+        item2.definePromocao(2.99);
+        IO.println("Item " + item2.nome + " está em promoção... Preço R$" + item2.precoComDesconto);
+
+        ItemCardapio item3 = new ItemCardapio(3, "Sorvete", "Sorvete bem gelado", 5.00, 3);
+
+        //IO.println("Nome: " + item3.nome);
     }
 }
